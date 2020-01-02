c8c853f4ec14fcc96964fdb5ce5bdee0687fce1c
arq-gabo <jgabrielguerral@gmail.com>
2020-01-02 00:12:23 -0500

    [FIX]Upload_files

--- online_store/order_management/admin.py
+++ online_store/order_management/admin.py
@@ -8,6 +8,14 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display=("name", "address", "phone")
     search_fields=("name", "phone")
 
+class ArticlesAdmin(admin.ModelAdmin):
+    list_filter=("section",)
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display=("number", "date")
+    list_filter=("date",)
+    date_hierarchy="date"
+
 admin.site.register(Customer, CustomerAdmin)
-admin.site.register(Articles)
-admin.site.register(Orders)
+admin.site.register(Articles, ArticlesAdmin)
+admin.site.register(Orders, OrdersAdmin)
